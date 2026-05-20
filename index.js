65db437b44347cc96327019b833d062c2d1a0ec2
souldev003 <mehrabhosen003@gmail.com>
2026-05-20 15:02:42 +0600

    update MongoDB connection to use environment variables and add rooms data and latest  6 rooms

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://zen-study:1jnpxYCLJ46wpVXQ@cluster0.ypnmfit.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 const app = express();
-const port = 5001;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,6 +24,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const db = client.db("zen-study");
+    const roomsCollection = db.collection("rooms");
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -34,6 +43,34 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/rooms", async (req, res) => {
+  try {
+    const rooms = await client
+      .db("zen-study")
+      .collection("rooms")
+      .find()
+      .toArray();
+    res.send(rooms);
+  } catch (error) {
+    res.status(500).send({ message: "Rooms fetch failed" });
+  }
+});
+
+app.get("/latest-rooms", async (req, res) => {
+  try {
+    const rooms = await client
+      .db("zen-study")
+      .collection("rooms")
+      .find()
+      .sort({ createdAt: -1 })
+      .limit(6)
+      .toArray();
+    res.send(rooms);
+  } catch (error) {
+    res.status(500).send({ message: "Rooms fetch failed" });
+  }
 });
 
 app.listen(port, () => {
